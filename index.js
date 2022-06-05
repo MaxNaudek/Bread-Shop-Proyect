7271950db92685b14a833d25be6801b39b95b936
Jacqueline Callejas <jcr.debaser@gmail.com>
2022-06-04 22:37:45 -0500

    Creación de card y primeros estilos

---
+++ index.js
@@ -0,0 +1,26 @@
+const menu = [
+    {
+      id: 1,
+      title: "chocolatín",
+      category: "pan especial",
+      price: 22.99,
+      img: "./images/chocolatin.jpg",
+      description: `Deliciosa masa de hojaldre rellena de chocolate 70% cacao`,
+    },
+    {
+        id: 2,
+        title: "dona",
+        category: "pan tradicional",
+        price: 17,
+        img: "./images/dona.jpg",
+        description: `La original rosquilla glaseada con un exquisito chocolate`,
+    },
+    {
+        id: 3,
+        title: "rosca de reyes",
+        category: "pan de temporada",
+        price: 230,
+        img: "./images/rosca.jpg",
+        description: `Disponible a partir del 1 de enero, rinde de 3 a 5 personas`,
+    }
+]  
